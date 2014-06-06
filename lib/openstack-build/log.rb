6059aa9719afca160c90b80d4c37c1d1de8b65a8
Rohith <gambol99@gmail.com>
2014-06-06 15:22:51 +0100

    - adding the fog dependency

--- lib/openstack-build/log.rb
+++ lib/openstack-build/log.rb
@@ -7,16 +7,16 @@
 require 'logger'
 module OpenstackBuild
 class Logger
-    class << self
-      attr_accessor :logger
+  class << self
+    attr_accessor :logger
 
-      def init options = {} 
-        self.logger = ::Logger.new( options[:std] || STDOUT)
-      end
+    def init options = {} 
+      self.logger = ::Logger.new( options[:std] || STDOUT)
+    end
 
-      def method_missing(m,*args,&block)
-        logger.send m, *args, &block if logger.respond_to? m
-      end
+    def method_missing(m,*args,&block)
+      logger.send m, *args, &block if logger.respond_to? m
     end
   end
+end
 end
